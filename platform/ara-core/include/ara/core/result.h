39276365ae54e8d760133e5e9d08002f06804f46
Razvan Roman <colector606@gmail.com>
2026-09-23 01:16:04 +0200

    feat: implement requirement AP-R3-CORE-005

---
+++ platform/ara-core/include/ara/core/result.h
@@ -0,0 +1,85 @@
+#ifndef ARA_CORE_RESULT_H_
+#define ARA_CORE_RESULT_H_
+
+#include "ara/core/core_fwd.h"
+#include "ara/core/error_code.h"
+
+#include <utility>
+#include <variant>
+
+namespace ara::core
+{
+  static constexpr std::size_t c_resultIdx = 0;
+  static constexpr std::size_t c_errorIdx = 1;
+
+  /* Implements: AP-R3-CORE-005: value or error storage */
+  /* Include initial factory/query support for CORE-006/007 */
+  template <typename T, typename E>
+  class Result final
+  {
+    public:
+      static Result FromValue(T value)
+      {
+        return Result{std::in_place_index<c_resultIdx>, std::move(value)};
+      }
+
+      static Result FromError(E error)
+      {
+        return Result{std::in_place_index<c_errorIdx>, std::move(error)};
+      }
+
+      [[nodiscard]] bool HasValue() const noexcept
+      {
+        return storage_.index() == 0;
+      }
+
+      Result(const Result&) = default;
+      Result(Result&&) = default;
+
+      /* Deffered until we design safe state replacement */
+      Result& operator=(const Result&) = delete;
+      Result& operator=(Result&&) = delete;
+      /* Deffered until we design safe state replacement */
+
+    private:
+      Result(std::in_place_index_t<c_resultIdx> tag, E error) : storage_{tag, std::move(value)} {}
+      Result(std::in_place_index_t<c_errorIdx> tag, E error) : storage_{tag, std::move(error)} {}
+
+      std::variant<T, E> storage_;
+  };
+
+  /* AP-R3-CORE-005: success without a value, or an error */
+  template <typename E>
+  class Result<void, E> final
+  {
+    public:
+      static Result FromValue()
+      {
+        return Result{};
+      }
+
+      static Result FromError(E error)
+      {
+        return Result{std::in_place_index<c_errorIdx>, std::move(error)};
+      }
+
+      [[nodiscard]] bool HasValue() const noexcept
+      {
+        return storage_.index() = 0;
+      }
+
+      Result(const Result&) = default;
+      Result(Result&&) = default;
+
+      Result& operator=(const Result&) = delete;
+      Result& operator=(Result&&) = delete;
+
+    private:
+      Result() : storage_{std::in_place_index<c_resultIdx>} {}
+      Result(std::in_place_index_t<c_errorIdx> tag, E error) : storage_{tag, std::move(error)} {}
+
+      std::variant<std::monostate, E> storage_;
+  };
+
+} // namespace ara::core
+#endif // ARA_CORE_RESULT_H_
